156b32931d87e928ffa050bf2d63d2d6066b356a
Tony <20404466+e-tony@users.noreply.github.com>
2020-07-09 22:57:12 +0200

    add random embedding weights

--- adviser/examples/qa/semanticparsing/reader.py
+++ adviser/examples/qa/semanticparsing/reader.py
@@ -11,7 +11,7 @@ def load_embs(file):
     return embs
 
 
-def get_data(df, embeddings, rel2idx, subset=None, shuffle=False):
+def get_data(df, embeddings, rel2idx, subset=None, shuffle=False, random=False):
     assert len(df.tokens) == len(embeddings)
     assert len(df.tokens) == len(df.relation)
     data = []
@@ -28,7 +28,10 @@ def get_data(df, embeddings, rel2idx, subset=None, shuffle=False):
     idxs = []
 
     for i in range(len(df.relation))[:sub]:
-        cls_emb = embeddings[i][0]
+        if random:
+            cls_emb = embeddings[i]
+        else:
+            cls_emb = embeddings[i][0]
         #         data.append((np.asarray(cls_emb, dtype=np.float32), rel2idx[df.relation[i]], df.id[i]))
         embs.append(np.asarray(cls_emb, dtype=np.float32))
         rels.append(rel2idx[df.relation[i]])

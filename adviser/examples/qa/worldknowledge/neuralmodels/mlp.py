7451dd6f4b7bd11dbf3c9dfaed1027b372be03a4
Tony <20404466+e-tony@users.noreply.github.com>
2020-07-09 13:42:25 +0200

    Create mlp.py

---
+++ adviser/examples/qa/worldknowledge/neuralmodels/mlp.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+import json
+import pickle
+import math
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NN(nn.Module):
+    def __init__(self, emb_dim, out_dim, dropout=0.25, n_hid1=256, n_hid2=256):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(emb_dim, n_hid1),
+            nn.ReLU(),
+            nn.BatchNorm1d(n_hid1),
+            nn.Dropout(dropout),
+            nn.Linear(n_hid1, n_hid2 // 4),
+            nn.ReLU(),
+            nn.BatchNorm1d(n_hid2 // 4),
+            nn.Dropout(dropout),
+            nn.Linear(n_hid2 // 4, out_dim),
+        )
+        for m in self.model:
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        return self.model(x)
